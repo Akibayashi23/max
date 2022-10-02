0a05284c8a1ddb833065e02779fd371fa403dbe3
Akibayashi23 <nissyokada23@gmail.com>
2022-10-02 16:22:56 +0900

    ユーザー追加

---
+++ app/serializers/todo_serializer.rb
@@ -0,0 +1,10 @@
+class TodoSerializer < ActiveModel::Serializer
+  # ここから記述
+    attributes :id, :title, :user_id, :username
+    belongs_to :user
+
+    def username
+      object.user.name
+    end
+  # ここまで記述
+  end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,6 @@
+class UserSerializer < ActiveModel::Serializer
+  # ここから記述
+    attributes :id, :name, :email
+    has_many :todos
+  # ここまで記述
+  end
