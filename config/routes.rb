a50bd65539c267252bb26e80afb1031e7727e926
Akibayashi23 <nissyokada23@gmail.com>
2022-10-15 23:48:51 +0900

    eventsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :todos, only: [:index, :create, :destroy]
     resources :users, only: [:index, :create]
+    resources :events, only: [:index, :show, :create, :update, :destroy]
   end
 end
